06c3b7d418b888064caec346059f3ace2cac69bb
fxf981 <yeyunet@gmail.com>
2024-12-14 09:38:12 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -96,15 +96,6 @@ EOF
   http_port $CADDY_HTTP_PORT
 }
 
-:8000 {
-  @x_ws {
-      path /vl
-      header Connection *Upgrade*
-      header Upgrade websocket
-    }
-  reverse_proxy @x_ws localhost:888
-}
-
 :$GRPC_PROXY_PORT {
   reverse_proxy {
     to localhost:$GRPC_PORT
